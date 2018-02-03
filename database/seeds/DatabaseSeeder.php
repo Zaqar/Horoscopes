0c298951012e6225aff2f652ab6731f17e2eaae5
Zaqar <zak.sams@mail.ru>
2018-02-03 18:35:59 +0400

    1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+//         $this->call(UsersTableSeeder::class);
 //          $this->call(ContentsTableSeeder::class);
-//         $this->call(CompatibilityHoroscopesTableSeeder::class);
-//         $this->call(ContnetsTableMinimalSeeder::class);
+//        $this->call(ZadiaksTableSeeder::class);
+         $this->call(CompatibilityHoroscopesTableSeeder::class);
+         $this->call(ContnetsTableMinimalSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 
 
+use Illuminate\Support\Facades\Route;
+
 Route::group(['middleware'=>'web'],function () {
     Route::get('/', 'IndexController@index')->name('index');
 
@@ -24,4 +26,26 @@ Route::group(['middleware'=>'web'],function () {
     })->name('compatibilityHoroscope');
 
     Route::post('/compatibilityHoroscope', 'IndexController@compatibilityHoroscopeShow')->name('compatibilityHoroscopePost');
+
+    Route::auth();
 });
+
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+
+    Route::group(['prefix'=>'horoscope'], function () {
+        Route::get('/', ['uses'=>'Admin/HoroscopeController@execute', 'as'=>'adminHoroscope']);
+        Route::match(['get','post'],'/add', ['uses'=>'Admin/HoroscopeAddController@execute', 'as'=>'horoscopeAdd']);
+        Route::match(['get','post','delete'],'/edit', ['uses'=>'HoroscopeEditController@execute', 'as'=>'horoscopeEdit']);
+    });
+
+    Route::group(['prefix'=>'compatibilityHoroscope'], function () {
+        Route::get('/', ['uses'=>'Admin/CompatibilityHoroscopeController@execute', 'as'=>'adminCompatibilityHoroscope']);
+        Route::match(['get','post'],'/add', ['uses'=>'Admin/CompatibilityHoroscopeAddController@execute', 'as'=>'compatibilityHoroscopeAdd']);
+        Route::match(['get','post','delete'],'/edit', ['uses'=>'CompatibilityHoroscopeEditController@execute', 'as'=>'compatibilityHoroscopeEdit']);
+    });
+});
+
+
